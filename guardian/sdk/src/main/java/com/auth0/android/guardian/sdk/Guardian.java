1adbde9b74fd598361541b77ccf5fdd65340c736
Nicolas Ulrich <nikolaseu@users.noreply.github.com>
2016-07-13 12:58:47 -0300

    Make Guardian parcelable

--- guardian/sdk/src/main/java/com/auth0/android/guardian/sdk/Guardian.java
+++ guardian/sdk/src/main/java/com/auth0/android/guardian/sdk/Guardian.java
@@ -23,13 +23,15 @@
 package com.auth0.android.guardian.sdk;
 
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.auth0.android.guardian.sdk.otp.TOTP;
 import com.auth0.android.guardian.sdk.otp.utils.Base32;
 
-public class Guardian {
+public class Guardian implements Parcelable {
 
     private final GuardianAPIClient client;
     private final String deviceName;
@@ -127,4 +129,37 @@ public class Guardian {
             return new Guardian(client, deviceName, gcmToken);
         }
     }
+
+    // PARCELABLE
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(client.getUrl());
+        dest.writeString(deviceName);
+        dest.writeString(gcmToken);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Guardian> CREATOR = new Parcelable.Creator<Guardian>() {
+        @Override
+        public Guardian createFromParcel(Parcel in) {
+            String url = in.readString();
+            String deviceName = in.readString();
+            String gcmToken = in.readString();
+            return new Builder()
+                    .url(url)
+                    .deviceName(deviceName)
+                    .gcmToken(gcmToken)
+                    .build();
+        }
+
+        @Override
+        public Guardian[] newArray(int size) {
+            return new Guardian[size];
+        }
+    };
 }
